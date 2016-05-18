brianchandotcom/liferay-portal

LPS-65153 ES classloader locks plugin jar on Windows; check don't remove
https://issues.liferay.com/browse/LPS-65153

modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchPluginManager.java
@@ -14,20 +14,17 @@
 
 package com.liferay.portal.search.elasticsearch.internal.connection;
 
-import com.liferay.portal.search.elasticsearch.internal.util.ResourceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.net.URI;
-import java.net.URL;
+import java.nio.file.Path;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.cli.Terminal;
 import org.elasticsearch.common.cli.Terminal.Verbosity;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.plugins.PluginManager;
 
 /**
  * @author Artur Aquino
@@ -35,37 +32,107 @@ import org.elasticsearch.plugins.PluginManager;
  */
 public class EmbeddedElasticsearchPluginManager {
 
-	public static void installPlugin(
-			String pluginName, String resourceName, Class<?> clazz,
-			Settings settings)
-		throws IOException {
+	public EmbeddedElasticsearchPluginManager(
+		String pluginName, String pluginsPath,
+		PluginManagerFactory pluginManagerFactory,
+		PluginZipFactory pluginZipFactory) {
 
-		File file = new File(settings.get("path.plugins"));
+		_pluginName = pluginName;
+		_pluginsPath = pluginsPath;
+		_pluginManagerFactory = pluginManagerFactory;
+		_pluginZipFactory = pluginZipFactory;
+	}
+
+	public void install() throws IOException {
+		if (isAlreadyInstalled()) {
+			return;
+		}
+
+		PluginZip pluginZip = createPluginZip();
+
+		try {
+			downloadAndExtract(pluginZip);
+		}
+		finally {
+			pluginZip.delete();
+		}
+	}
+
+	protected PluginZip createPluginZip() throws IOException {
+		String version = Version.CURRENT.toString();
+
+		String resource = "/plugins/" + _pluginName + "-" + version + ".zip";
+
+		return _pluginZipFactory.createPluginZip(resource);
+	}
+
+	protected void downloadAndExtract(PluginZip pluginZip) throws IOException {
+		File file = new File(_pluginsPath);
 
 		file.mkdirs();
 
-		File tempFile = ResourceUtil.getResourceAsTempFile(clazz, resourceName);
+		PluginManager pluginManager = _pluginManagerFactory.createPluginManager(
+			pluginZip);
+
+		Terminal terminal = Terminal.DEFAULT;
+
+		terminal.verbosity(Verbosity.SILENT);
 
 		try {
-			URI uri = tempFile.toURI();
-
-			URL url = uri.toURL();
-
-			PluginManager pluginManager = new PluginManager(
-				new Environment(settings), url, PluginManager.OutputMode.SILENT,
-				TimeValue.timeValueMinutes(1));
-
-			Terminal terminal = Terminal.DEFAULT;
-
-			terminal.verbosity(Verbosity.SILENT);
-
-			pluginManager.removePlugin(pluginName, terminal);
-
-			pluginManager.downloadAndExtract(pluginName, terminal, true);
+			pluginManager.downloadAndExtract(_pluginName, terminal, true);
 		}
-		finally {
-			tempFile.delete();
+		catch (IOException ioe) {
+			if (!handle(ioe)) {
+				throw ioe;
+			}
 		}
 	}
+
+	protected boolean handle(IOException ioe) {
+		String message = ioe.getMessage();
+
+		if (message == null) {
+			return false;
+		}
+
+		if (message.contains(
+				"already exists. To update the plugin, uninstall it first")) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Plugin " + _pluginName + " already installed. Skipping...",
+					ioe);
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected boolean isAlreadyInstalled() throws IOException {
+		PluginManager pluginManager =
+			_pluginManagerFactory.createPluginManager();
+
+		Path[] paths = pluginManager.getListInstalledPlugins();
+
+		if (paths != null) {
+			for (Path path : paths) {
+				if (path.endsWith(_pluginName)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EmbeddedElasticsearchPluginManager.class);
+
+	private final PluginManagerFactory _pluginManagerFactory;
+	private final String _pluginName;
+	private final String _pluginsPath;
+	private final PluginZipFactory _pluginZipFactory;
 
 }

modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginManager.java
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+
+import org.elasticsearch.common.cli.Terminal;
+
+/**
+ * @author Artur Aquino
+ * @author André de Oliveira
+ */
+public interface PluginManager {
+
+	public void downloadAndExtract(
+			String name, Terminal terminal, boolean batch)
+		throws IOException;
+
+	public Path[] getListInstalledPlugins() throws IOException;
+
+}

modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginManagerImpl.java
@@ -1,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.nio.file.Path;
+
+import org.elasticsearch.common.cli.Terminal;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.plugins.PluginManager.OutputMode;
+
+/**
+ * @author Artur Aquino
+ * @author André de Oliveira
+ */
+public class PluginManagerImpl implements PluginManager {
+
+	public PluginManagerImpl(
+		Environment environment, URL url, OutputMode outputMode,
+		TimeValue timeout) {
+
+		_pluginManager = new org.elasticsearch.plugins.PluginManager(
+			environment, url, outputMode, timeout);
+	}
+
+	@Override
+	public void downloadAndExtract(
+			String name, Terminal terminal, boolean batch)
+		throws IOException {
+
+		_pluginManager.downloadAndExtract(name, terminal, batch);
+	}
+
+	@Override
+	public Path[] getListInstalledPlugins() throws IOException {
+		return _pluginManager.getListInstalledPlugins();
+	}
+
+	private final org.elasticsearch.plugins.PluginManager _pluginManager;
+
+}

modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginZipFactory.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+/**
+ * @author André de Oliveira
+ */
+public interface PluginZipFactory {
+
+	public PluginZip createPluginZip(String resource) throws IOException;
+
+}

modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginZipFactoryImpl.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import com.liferay.portal.search.elasticsearch.internal.util.ResourceUtil;
+
+import java.io.IOException;
+
+/**
+ * @author André de Oliveira
+ */
+public class PluginZipFactoryImpl implements PluginZipFactory {
+
+	@Override
+	public PluginZip createPluginZip(String resource) {
+		try {
+			return new PluginZipImpl(
+				ResourceUtil.getResourceAsTempFile(getClass(), resource));
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				"Unable to write temporary plugin zip file from resource " +
+					resource,
+				ioe);
+		}
+	}
+
+}

vikkisy/com-liferay-portal-search

portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchPluginManager.java
@@ -14,20 +14,17 @@
 
 package com.liferay.portal.search.elasticsearch.internal.connection;
 
-import com.liferay.portal.search.elasticsearch.internal.util.ResourceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.net.URI;
-import java.net.URL;
+import java.nio.file.Path;
 
+import org.elasticsearch.Version;
 import org.elasticsearch.common.cli.Terminal;
 import org.elasticsearch.common.cli.Terminal.Verbosity;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.plugins.PluginManager;
 
 /**
  * @author Artur Aquino
@@ -35,37 +32,108 @@ import org.elasticsearch.plugins.PluginManager;
  */
 public class EmbeddedElasticsearchPluginManager {
 
-	public static void installPlugin(
-			String pluginName, String resourceName, Class<?> clazz,
-			Settings settings)
-		throws IOException {
+	public EmbeddedElasticsearchPluginManager(
+		String pluginName, String pluginsPathString,
+		PluginManagerFactory pluginManagerFactory,
+		PluginZipFactory pluginZipFactory) {
 
-		File file = new File(settings.get("path.plugins"));
+		_pluginName = pluginName;
+		_pluginsPathString = pluginsPathString;
+		_pluginManagerFactory = pluginManagerFactory;
+		_pluginZipFactory = pluginZipFactory;
+	}
+
+	public void install() throws IOException {
+		if (isAlreadyInstalled()) {
+			return;
+		}
+
+		PluginZip pluginZip = createPluginZip();
+
+		try {
+			downloadAndExtract(pluginZip);
+		}
+		finally {
+			pluginZip.delete();
+		}
+	}
+
+	protected PluginZip createPluginZip() throws IOException {
+		String versionString = Version.CURRENT.toString();
+
+		String resourceName =
+			"/plugins/" + _pluginName + "-" + versionString + ".zip";
+
+		return _pluginZipFactory.createPluginZip(resourceName);
+	}
+
+	protected void downloadAndExtract(PluginZip pluginZip) throws IOException {
+		File file = new File(_pluginsPathString);
 
 		file.mkdirs();
 
-		File tempFile = ResourceUtil.getResourceAsTempFile(clazz, resourceName);
+		PluginManager pluginManager = _pluginManagerFactory.createPluginManager(
+			pluginZip);
+
+		Terminal terminal = Terminal.DEFAULT;
+
+		terminal.verbosity(Verbosity.SILENT);
 
 		try {
-			URI uri = tempFile.toURI();
-
-			URL url = uri.toURL();
-
-			PluginManager pluginManager = new PluginManager(
-				new Environment(settings), url, PluginManager.OutputMode.SILENT,
-				TimeValue.timeValueMinutes(1));
-
-			Terminal terminal = Terminal.DEFAULT;
-
-			terminal.verbosity(Verbosity.SILENT);
-
-			pluginManager.removePlugin(pluginName, terminal);
-
-			pluginManager.downloadAndExtract(pluginName, terminal, true);
+			pluginManager.downloadAndExtract(_pluginName, terminal, true);
 		}
-		finally {
-			tempFile.delete();
+		catch (IOException ioe) {
+			if (!handle(ioe)) {
+				throw ioe;
+			}
 		}
 	}
+
+	protected boolean handle(IOException ioe) {
+		String message = ioe.getMessage();
+
+		if (message == null) {
+			return false;
+		}
+
+		if (message.contains(
+				"already exists. To update the plugin, uninstall it first")) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Plugin " + _pluginName + " already installed. Skipping...",
+					ioe);
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected boolean isAlreadyInstalled() throws IOException {
+		PluginManager pluginManager =
+			_pluginManagerFactory.createPluginManager();
+
+		Path[] paths = pluginManager.getInstalledPluginsPaths();
+
+		if (paths != null) {
+			for (Path path : paths) {
+				if (path.endsWith(_pluginName)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EmbeddedElasticsearchPluginManager.class);
+
+	private final PluginManagerFactory _pluginManagerFactory;
+	private final String _pluginName;
+	private final String _pluginsPathString;
+	private final PluginZipFactory _pluginZipFactory;
 
 }

portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginManager.java
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+
+import org.elasticsearch.common.cli.Terminal;
+
+/**
+ * @author Artur Aquino
+ * @author André de Oliveira
+ */
+public interface PluginManager {
+
+	public void downloadAndExtract(
+			String name, Terminal terminal, boolean batch)
+		throws IOException;
+
+	public Path[] getInstalledPluginsPaths() throws IOException;
+
+}

portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginManagerImpl.java
@@ -1,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.nio.file.Path;
+
+import org.elasticsearch.common.cli.Terminal;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.plugins.PluginManager.OutputMode;
+
+/**
+ * @author Artur Aquino
+ * @author André de Oliveira
+ */
+public class PluginManagerImpl implements PluginManager {
+
+	public PluginManagerImpl(
+		Environment environment, URL url, OutputMode outputMode,
+		TimeValue timeout) {
+
+		_pluginManager = new org.elasticsearch.plugins.PluginManager(
+			environment, url, outputMode, timeout);
+	}
+
+	@Override
+	public void downloadAndExtract(
+			String name, Terminal terminal, boolean batch)
+		throws IOException {
+
+		_pluginManager.downloadAndExtract(name, terminal, batch);
+	}
+
+	@Override
+	public Path[] getInstalledPluginsPaths() throws IOException {
+		return _pluginManager.getListInstalledPlugins();
+	}
+
+	private final org.elasticsearch.plugins.PluginManager _pluginManager;
+
+}

portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginZipFactory.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import java.io.IOException;
+
+/**
+ * @author André de Oliveira
+ */
+public interface PluginZipFactory {
+
+	public PluginZip createPluginZip(String resourceName) throws IOException;
+
+}

portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/PluginZipFactoryImpl.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.connection;
+
+import com.liferay.portal.search.elasticsearch.internal.util.ResourceUtil;
+
+import java.io.IOException;
+
+/**
+ * @author André de Oliveira
+ */
+public class PluginZipFactoryImpl implements PluginZipFactory {
+
+	@Override
+	public PluginZip createPluginZip(String resourceName) {
+		try {
+			return new PluginZipImpl(
+				ResourceUtil.getResourceAsTempFile(getClass(), resourceName));
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				"Unable to write temporary plugin zip file from resource " +
+					resourceName,
+				ioe);
+		}
+	}
+
+}