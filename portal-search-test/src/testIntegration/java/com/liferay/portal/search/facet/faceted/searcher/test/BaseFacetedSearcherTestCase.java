brianchandotcom/liferay-portal

LPS-72144 portal-search-test: Package was renamed
https://issues.liferay.com/browse/LPS-72144

modules/apps/foundation/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/BaseFacetedSearcherTestCase.java
@@ -25,9 +25,9 @@ import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcher;
 import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcherManager;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.search.internal.test.util.UserSearchFixture;
 import com.liferay.portal.search.test.util.AssertUtils;
 import com.liferay.portal.search.test.util.TermCollectorUtil;
-import com.liferay.portal.search.test.util.UserSearchFixture;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 

vikkisy/com-liferay-portal-search

portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/BaseFacetedSearcherTestCase.java
@@ -25,9 +25,9 @@ import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcher;
 import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcherManager;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.search.test.internal.util.UserSearchFixture;
 import com.liferay.portal.search.test.util.AssertUtils;
 import com.liferay.portal.search.test.util.TermCollectorUtil;
-import com.liferay.portal.search.test.util.UserSearchFixture;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 