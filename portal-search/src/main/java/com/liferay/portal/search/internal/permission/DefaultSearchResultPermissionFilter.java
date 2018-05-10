brianchandotcom/liferay-portal

LPS-74395 portal-search: Prevent too many results while filtering permissions
- limit the result window size of a search query
- only retain documents while they’re needed
- merge superclass BaseSearchResultPermissionFilter (no other subclasses)

https://issues.liferay.com/browse/LPS-74395

modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/DefaultSearchResultPermissionFilter.java
@@ -16,17 +16,23 @@ package com.liferay.portal.search.internal.permission;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.search.SearchPaginationUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.HitsImpl;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.RelatedEntryIndexer;
-import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistryUtil;
+import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilter;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilterSearcher;
 import com.liferay.portal.kernel.search.facet.Facet;
 import com.liferay.portal.kernel.search.facet.FacetPostProcessor;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -34,35 +40,86 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.configuration.DefaultSearchResultPermissionFilterConfiguration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Tina Tian
  */
 @ProviderType
 public class DefaultSearchResultPermissionFilter
-	extends BaseSearchResultPermissionFilter {
+	implements SearchResultPermissionFilter {
 
 	public DefaultSearchResultPermissionFilter(
-		SearchExecutor searchExecutor, PermissionChecker permissionChecker) {
+		FacetPostProcessor facetPostProcessor, IndexerRegistry indexerRegistry,
+		PermissionChecker permissionChecker, Props props,
+		RelatedEntryIndexerRegistry relatedEntryIndexerRegistry,
+		SearchResultPermissionFilterSearcher
+			searchResultPermissionFilterSearcher,
+		DefaultSearchResultPermissionFilterConfiguration
+			defaultSearchResultPermissionFilterConfiguration) {
 
-		_searchExecutor = searchExecutor;
+		_facetPostProcessor = facetPostProcessor;
+		_indexerRegistry = indexerRegistry;
 		_permissionChecker = permissionChecker;
-	}
+		_relatedEntryIndexerRegistry = relatedEntryIndexerRegistry;
+		_searchResultPermissionFilterSearcher =
+			searchResultPermissionFilterSearcher;
 
-	public interface SearchExecutor {
+		_searchQueryResultWindowLimit =
+			defaultSearchResultPermissionFilterConfiguration.
+				searchQueryResultWindowLimit();
 
-		public Hits search(SearchContext searchContext) throws SearchException;
-
+		setProps(props);
 	}
 
 	@Override
+	public Hits search(SearchContext searchContext) throws SearchException {
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		if (!queryConfig.isAllFieldsSelected()) {
+			queryConfig.setSelectedFieldNames(
+				getSelectedFieldNames(queryConfig.getSelectedFieldNames()));
+		}
+
+		int end = searchContext.getEnd();
+		int start = searchContext.getStart();
+
+		if ((end == QueryUtil.ALL_POS) && (start == QueryUtil.ALL_POS)) {
+			Hits hits = getHits(searchContext);
+
+			if (!isGroupAdmin(searchContext)) {
+				filterHits(hits, searchContext);
+			}
+
+			return hits;
+		}
+
+		if ((start < 0) || (start > end)) {
+			return new HitsImpl();
+		}
+
+		if (isGroupAdmin(searchContext)) {
+			return getHits(searchContext);
+		}
+
+		SlidingWindowSearcher slidingWindowSearcher =
+			new SlidingWindowSearcher();
+
+		return slidingWindowSearcher.search(start, end, searchContext);
+	}
+
 	protected void filterHits(Hits hits, SearchContext searchContext) {
 		List<Document> docs = new ArrayList<>();
 		List<Document> excludeDocs = new ArrayList<>();
@@ -90,14 +147,10 @@ public class DefaultSearchResultPermissionFilter
 		}
 
 		if (!excludeDocs.isEmpty()) {
-			FacetPostProcessor facetPostProcessor = _facetPostProcessor;
+			Map<String, Facet> facets = searchContext.getFacets();
 
-			if (facetPostProcessor != null) {
-				Map<String, Facet> facets = searchContext.getFacets();
-
-				for (Facet facet : facets.values()) {
-					facetPostProcessor.exclude(excludeDocs, facet);
-				}
+			for (Facet facet : facets.values()) {
+				_facetPostProcessor.exclude(excludeDocs, facet);
 			}
 		}
 
@@ -109,12 +162,41 @@ public class DefaultSearchResultPermissionFilter
 		hits.setLength(hits.getLength() - excludeDocs.size());
 	}
 
-	@Override
 	protected Hits getHits(SearchContext searchContext) throws SearchException {
-		return _searchExecutor.search(searchContext);
+		if ((searchContext != null) &&
+			(searchContext.getEnd() != QueryUtil.ALL_POS)) {
+
+			int end = searchContext.getEnd();
+			int start = searchContext.getStart();
+
+			if (start == QueryUtil.ALL_POS) {
+				start = 0;
+			}
+
+			int searchResultWindow = end - start;
+
+			if (searchResultWindow > _searchQueryResultWindowLimit) {
+				throw new SearchException(
+					StringBundler.concat(
+						"The search query is requesting for a range of ",
+						String.valueOf(searchResultWindow),
+						" search results. This exceeds the currently ",
+						"configured Search Query Result Window Limit: ",
+						String.valueOf(_searchQueryResultWindowLimit)));
+			}
+		}
+
+		return _searchResultPermissionFilterSearcher.search(searchContext);
 	}
 
-	@Override
+	protected String[] getSelectedFieldNames(String[] selectedFieldNames) {
+		Set<String> set = SetUtil.fromArray(selectedFieldNames);
+
+		Collections.addAll(set, _PERMISSION_SELECTED_FIELD_NAMES);
+
+		return set.toArray(new String[set.size()]);
+	}
+
 	protected boolean isGroupAdmin(SearchContext searchContext) {
 		long groupId = GetterUtil.getLong(
 			searchContext.getAttribute(Field.GROUP_ID));
@@ -128,6 +210,14 @@ public class DefaultSearchResultPermissionFilter
 		}
 
 		return true;
+	}
+
+	protected void setProps(Props props) {
+		_props = props;
+
+		_indexPermissionFilterSearchAmplificationFactor = GetterUtil.getDouble(
+			_props.get(
+				PropsKeys.INDEX_PERMISSION_FILTER_SEARCH_AMPLIFICATION_FACTOR));
 	}
 
 	private boolean _isIncludeDocument(
@@ -146,7 +236,7 @@ public class DefaultSearchResultPermissionFilter
 
 		String entryClassName = document.get(Field.ENTRY_CLASS_NAME);
 
-		Indexer<?> indexer = IndexerRegistryUtil.getIndexer(entryClassName);
+		Indexer<?> indexer = _indexerRegistry.getIndexer(entryClassName);
 
 		if (indexer == null) {
 			return true;
@@ -165,15 +255,18 @@ public class DefaultSearchResultPermissionFilter
 					ActionKeys.VIEW)) {
 
 				List<RelatedEntryIndexer> relatedEntryIndexers =
-					RelatedEntryIndexerRegistryUtil.getRelatedEntryIndexers(
+					_relatedEntryIndexerRegistry.getRelatedEntryIndexers(
 						entryClassName);
 
 				if (ListUtil.isNotEmpty(relatedEntryIndexers)) {
 					for (RelatedEntryIndexer relatedEntryIndexer :
 							relatedEntryIndexers) {
 
-						relatedEntryIndexer.isVisibleRelatedEntry(
-							entryClassPK, status);
+						if (!relatedEntryIndexer.isVisibleRelatedEntry(
+								entryClassPK, status)) {
+
+							return false;
+						}
 					}
 				}
 
@@ -189,15 +282,186 @@ public class DefaultSearchResultPermissionFilter
 		return false;
 	}
 
+	private static final String[] _PERMISSION_SELECTED_FIELD_NAMES =
+		{Field.COMPANY_ID, Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK};
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		DefaultSearchResultPermissionFilter.class);
 
-	private static volatile FacetPostProcessor _facetPostProcessor =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			FacetPostProcessor.class, DefaultSearchResultPermissionFilter.class,
-			"_facetPostProcessor", false, true);
-
+	private final FacetPostProcessor _facetPostProcessor;
+	private final IndexerRegistry _indexerRegistry;
+	private double _indexPermissionFilterSearchAmplificationFactor;
 	private final PermissionChecker _permissionChecker;
-	private final SearchExecutor _searchExecutor;
+	private Props _props;
+	private final RelatedEntryIndexerRegistry _relatedEntryIndexerRegistry;
+	private final int _searchQueryResultWindowLimit;
+	private final SearchResultPermissionFilterSearcher
+		_searchResultPermissionFilterSearcher;
+
+	private class SlidingWindowSearcher {
+
+		public Hits search(int start, int end, SearchContext searchContext)
+			throws SearchException {
+
+			double amplificationFactor = 1.0;
+			int excludedDocsSize = 0;
+			int filteredDocsCount = 0;
+			int hitsSize = 0;
+			int offset = 0;
+			long startTime = 0;
+
+			while (true) {
+				int count = end - documents.size();
+
+				int amplifiedCount = Math.min(
+					(int)Math.ceil(count * amplificationFactor),
+					_searchQueryResultWindowLimit);
+
+				int amplifiedEnd = offset + amplifiedCount;
+
+				searchContext.setEnd(amplifiedEnd);
+
+				searchContext.setStart(offset);
+
+				Hits hits = getHits(searchContext);
+
+				if (startTime == 0) {
+					hitsSize = hits.getLength();
+					startTime = hits.getStart();
+				}
+
+				Document[] oldDocs = hits.getDocs();
+
+				filterHits(hits, searchContext);
+
+				Document[] newDocs = hits.getDocs();
+
+				excludedDocsSize += oldDocs.length - newDocs.length;
+
+				filteredDocsCount += newDocs.length;
+
+				collectHits(hits, filteredDocsCount, start, end);
+
+				if ((newDocs.length >= count) ||
+					(oldDocs.length < amplifiedCount) ||
+					(amplifiedEnd >= hitsSize)) {
+
+					updateDocuments(filteredDocsCount, start, end);
+
+					updateHits(hits, hitsSize - excludedDocsSize, startTime);
+
+					return hits;
+				}
+
+				offset = amplifiedEnd;
+
+				amplificationFactor = _getAmplificationFactor(
+					documents.size(), offset);
+			}
+		}
+
+		protected void collectHits(
+			Hits hits, int accumulatedCount, int start, int end) {
+
+			int delta = end - start;
+			Document[] docs = hits.getDocs();
+
+			int remaining = docs.length;
+
+			if ((accumulatedCount > start) && (documents.size() < delta)) {
+				int previousAccumulatedCount = accumulatedCount - docs.length;
+
+				int docsStart = 0;
+
+				if (start > previousAccumulatedCount) {
+					docsStart = start - previousAccumulatedCount;
+				}
+
+				int docsEnd = docsStart + (delta - documents.size());
+
+				if (docsEnd > docs.length) {
+					docsEnd = docs.length;
+				}
+
+				for (int i = docsStart; i < docsEnd; i++) {
+					documents.add(docs[i]);
+
+					scores.add(hits.score(i));
+				}
+
+				remaining -= docsEnd;
+
+				if (remaining == 0) {
+					return;
+				}
+			}
+
+			for (int i = docs.length - remaining; i < docs.length; i++) {
+				if (standbyDocuments.size() == delta) {
+					standbyDocuments.remove(0);
+					standbyScores.remove(0);
+				}
+
+				standbyDocuments.add(docs[i]);
+				standbyScores.add(hits.score(i));
+			}
+		}
+
+		protected void updateDocuments(
+			int accumulatedCount, int start, int end) {
+
+			if ((start < accumulatedCount) || standbyDocuments.isEmpty()) {
+				return;
+			}
+
+			documents.addAll(0, standbyDocuments);
+			scores.addAll(0, standbyScores);
+
+			int delta = end - start;
+			int docsStart = start - accumulatedCount;
+
+			int docsEnd = docsStart + delta;
+
+			int[] startAndEnd = SearchPaginationUtil.calculateStartAndEnd(
+				docsStart, docsEnd, documents.size());
+
+			docsStart = startAndEnd[0];
+
+			docsEnd = startAndEnd[1];
+
+			for (int i = 0; i < documents.size(); i++) {
+				if ((i < docsStart) || (i >= docsEnd)) {
+					documents.remove(i);
+					scores.remove(i);
+				}
+			}
+		}
+
+		protected void updateHits(Hits hits, int size, long startTime) {
+			hits.setDocs(documents.toArray(new Document[documents.size()]));
+			hits.setScores(ArrayUtil.toFloatArray(scores));
+			hits.setLength(size);
+			hits.setSearchTime(
+				(float)(System.currentTimeMillis() - startTime) / Time.SECOND);
+		}
+
+		protected List<Document> documents = new ArrayList<>();
+		protected List<Float> scores = new ArrayList<>();
+		protected List<Document> standbyDocuments = new ArrayList<>();
+		protected List<Float> standbyScores = new ArrayList<>();
+
+		private double _getAmplificationFactor(
+			double totalViewable, double total) {
+
+			if (totalViewable == 0) {
+				return _indexPermissionFilterSearchAmplificationFactor;
+			}
+
+			return Math.min(
+				1.0 / (totalViewable / total),
+				_indexPermissionFilterSearchAmplificationFactor);
+		}
+
+	}
 
 }

vikkisy/com-liferay-portal-search

portal-search/src/main/java/com/liferay/portal/search/internal/permission/DefaultSearchResultPermissionFilter.java
@@ -16,17 +16,22 @@ package com.liferay.portal.search.internal.permission;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.search.SearchPaginationUtil;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.HitsImpl;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.RelatedEntryIndexer;
-import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistryUtil;
+import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilter;
 import com.liferay.portal.kernel.search.facet.Facet;
 import com.liferay.portal.kernel.search.facet.FacetPostProcessor;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -34,35 +39,85 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.configuration.DefaultSearchResultPermissionFilterConfiguration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 /**
  * @author Tina Tian
  */
 @ProviderType
 public class DefaultSearchResultPermissionFilter
-	extends BaseSearchResultPermissionFilter {
+	implements SearchResultPermissionFilter {
 
 	public DefaultSearchResultPermissionFilter(
-		SearchExecutor searchExecutor, PermissionChecker permissionChecker) {
+		FacetPostProcessor facetPostProcessor, IndexerRegistry indexerRegistry,
+		PermissionChecker permissionChecker, Props props,
+		RelatedEntryIndexerRegistry relatedEntryIndexerRegistry,
+		Function<SearchContext, Hits> searchFunction,
+		DefaultSearchResultPermissionFilterConfiguration
+			defaultSearchResultPermissionFilterConfiguration) {
 
-		_searchExecutor = searchExecutor;
+		_facetPostProcessor = facetPostProcessor;
+		_indexerRegistry = indexerRegistry;
 		_permissionChecker = permissionChecker;
-	}
+		_relatedEntryIndexerRegistry = relatedEntryIndexerRegistry;
+		_searchFunction = searchFunction;
 
-	public interface SearchExecutor {
+		_searchQueryResultWindowLimit =
+			defaultSearchResultPermissionFilterConfiguration.
+				searchQueryResultWindowLimit();
 
-		public Hits search(SearchContext searchContext) throws SearchException;
-
+		setProps(props);
 	}
 
 	@Override
+	public Hits search(SearchContext searchContext) {
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		if (!queryConfig.isAllFieldsSelected()) {
+			queryConfig.setSelectedFieldNames(
+				getSelectedFieldNames(queryConfig.getSelectedFieldNames()));
+		}
+
+		int end = searchContext.getEnd();
+		int start = searchContext.getStart();
+
+		if ((end == QueryUtil.ALL_POS) && (start == QueryUtil.ALL_POS)) {
+			Hits hits = getHits(searchContext);
+
+			if (!isGroupAdmin(searchContext)) {
+				filterHits(hits, searchContext);
+			}
+
+			return hits;
+		}
+
+		if ((start < 0) || (start > end)) {
+			return new HitsImpl();
+		}
+
+		if (isGroupAdmin(searchContext)) {
+			return getHits(searchContext);
+		}
+
+		SlidingWindowSearcher slidingWindowSearcher =
+			new SlidingWindowSearcher();
+
+		return slidingWindowSearcher.search(start, end, searchContext);
+	}
+
 	protected void filterHits(Hits hits, SearchContext searchContext) {
 		List<Document> docs = new ArrayList<>();
 		List<Document> excludeDocs = new ArrayList<>();
@@ -90,14 +145,10 @@ public class DefaultSearchResultPermissionFilter
 		}
 
 		if (!excludeDocs.isEmpty()) {
-			FacetPostProcessor facetPostProcessor = _facetPostProcessor;
+			Map<String, Facet> facets = searchContext.getFacets();
 
-			if (facetPostProcessor != null) {
-				Map<String, Facet> facets = searchContext.getFacets();
-
-				for (Facet facet : facets.values()) {
-					facetPostProcessor.exclude(excludeDocs, facet);
-				}
+			for (Facet facet : facets.values()) {
+				_facetPostProcessor.exclude(excludeDocs, facet);
 			}
 		}
 
@@ -109,12 +160,40 @@ public class DefaultSearchResultPermissionFilter
 		hits.setLength(hits.getLength() - excludeDocs.size());
 	}
 
-	@Override
-	protected Hits getHits(SearchContext searchContext) throws SearchException {
-		return _searchExecutor.search(searchContext);
+	protected Hits getHits(SearchContext searchContext) {
+		if ((searchContext != null) &&
+			(searchContext.getEnd() != QueryUtil.ALL_POS)) {
+
+			int end = searchContext.getEnd();
+			int start = searchContext.getStart();
+
+			if (start == QueryUtil.ALL_POS) {
+				start = 0;
+			}
+
+			int searchResultWindow = end - start;
+
+			if (searchResultWindow > _searchQueryResultWindowLimit) {
+				throw new SystemException(
+					StringBundler.concat(
+						"Search result window size of ",
+						String.valueOf(searchResultWindow),
+						" exceeds the configured limit of ",
+						String.valueOf(_searchQueryResultWindowLimit)));
+			}
+		}
+
+		return _searchFunction.apply(searchContext);
 	}
 
-	@Override
+	protected String[] getSelectedFieldNames(String[] selectedFieldNames) {
+		Set<String> set = SetUtil.fromArray(selectedFieldNames);
+
+		Collections.addAll(set, _PERMISSION_SELECTED_FIELD_NAMES);
+
+		return set.toArray(new String[set.size()]);
+	}
+
 	protected boolean isGroupAdmin(SearchContext searchContext) {
 		long groupId = GetterUtil.getLong(
 			searchContext.getAttribute(Field.GROUP_ID));
@@ -128,6 +207,14 @@ public class DefaultSearchResultPermissionFilter
 		}
 
 		return true;
+	}
+
+	protected void setProps(Props props) {
+		_props = props;
+
+		_indexPermissionFilterSearchAmplificationFactor = GetterUtil.getDouble(
+			_props.get(
+				PropsKeys.INDEX_PERMISSION_FILTER_SEARCH_AMPLIFICATION_FACTOR));
 	}
 
 	private boolean _isIncludeDocument(
@@ -146,7 +233,7 @@ public class DefaultSearchResultPermissionFilter
 
 		String entryClassName = document.get(Field.ENTRY_CLASS_NAME);
 
-		Indexer<?> indexer = IndexerRegistryUtil.getIndexer(entryClassName);
+		Indexer<?> indexer = _indexerRegistry.getIndexer(entryClassName);
 
 		if (indexer == null) {
 			return true;
@@ -165,15 +252,18 @@ public class DefaultSearchResultPermissionFilter
 					ActionKeys.VIEW)) {
 
 				List<RelatedEntryIndexer> relatedEntryIndexers =
-					RelatedEntryIndexerRegistryUtil.getRelatedEntryIndexers(
+					_relatedEntryIndexerRegistry.getRelatedEntryIndexers(
 						entryClassName);
 
 				if (ListUtil.isNotEmpty(relatedEntryIndexers)) {
 					for (RelatedEntryIndexer relatedEntryIndexer :
 							relatedEntryIndexers) {
 
-						relatedEntryIndexer.isVisibleRelatedEntry(
-							entryClassPK, status);
+						if (!relatedEntryIndexer.isVisibleRelatedEntry(
+								entryClassPK, status)) {
+
+							return false;
+						}
 					}
 				}
 
@@ -189,15 +279,183 @@ public class DefaultSearchResultPermissionFilter
 		return false;
 	}
 
+	private static final String[] _PERMISSION_SELECTED_FIELD_NAMES =
+		{Field.COMPANY_ID, Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK};
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		DefaultSearchResultPermissionFilter.class);
 
-	private static volatile FacetPostProcessor _facetPostProcessor =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			FacetPostProcessor.class, DefaultSearchResultPermissionFilter.class,
-			"_facetPostProcessor", false, true);
-
+	private final FacetPostProcessor _facetPostProcessor;
+	private final IndexerRegistry _indexerRegistry;
+	private double _indexPermissionFilterSearchAmplificationFactor;
 	private final PermissionChecker _permissionChecker;
-	private final SearchExecutor _searchExecutor;
+	private Props _props;
+	private final RelatedEntryIndexerRegistry _relatedEntryIndexerRegistry;
+	private final Function<SearchContext, Hits> _searchFunction;
+	private final int _searchQueryResultWindowLimit;
+
+	private class SlidingWindowSearcher {
+
+		public Hits search(int start, int end, SearchContext searchContext) {
+			double amplificationFactor = 1.0;
+			int excludedDocsSize = 0;
+			int filteredDocsCount = 0;
+			int hitsSize = 0;
+			int offset = 0;
+			long startTime = 0;
+
+			while (true) {
+				int count = end - documents.size();
+
+				int amplifiedCount = Math.min(
+					(int)Math.ceil(count * amplificationFactor),
+					_searchQueryResultWindowLimit);
+
+				int amplifiedEnd = offset + amplifiedCount;
+
+				searchContext.setEnd(amplifiedEnd);
+
+				searchContext.setStart(offset);
+
+				Hits hits = getHits(searchContext);
+
+				if (startTime == 0) {
+					hitsSize = hits.getLength();
+					startTime = hits.getStart();
+				}
+
+				Document[] oldDocs = hits.getDocs();
+
+				filterHits(hits, searchContext);
+
+				Document[] newDocs = hits.getDocs();
+
+				excludedDocsSize += oldDocs.length - newDocs.length;
+
+				filteredDocsCount += newDocs.length;
+
+				collectHits(hits, filteredDocsCount, start, end);
+
+				if ((newDocs.length >= count) ||
+					(oldDocs.length < amplifiedCount) ||
+					(amplifiedEnd >= hitsSize)) {
+
+					updateDocuments(filteredDocsCount, start, end);
+
+					updateHits(hits, hitsSize - excludedDocsSize, startTime);
+
+					return hits;
+				}
+
+				offset = amplifiedEnd;
+
+				amplificationFactor = _getAmplificationFactor(
+					documents.size(), offset);
+			}
+		}
+
+		protected void collectHits(
+			Hits hits, int accumulatedCount, int start, int end) {
+
+			int delta = end - start;
+			Document[] docs = hits.getDocs();
+
+			int remaining = docs.length;
+
+			if ((accumulatedCount > start) && (documents.size() < delta)) {
+				int previousAccumulatedCount = accumulatedCount - docs.length;
+
+				int docsStart = 0;
+
+				if (start > previousAccumulatedCount) {
+					docsStart = start - previousAccumulatedCount;
+				}
+
+				int docsEnd = docsStart + (delta - documents.size());
+
+				if (docsEnd > docs.length) {
+					docsEnd = docs.length;
+				}
+
+				for (int i = docsStart; i < docsEnd; i++) {
+					documents.add(docs[i]);
+
+					scores.add(hits.score(i));
+				}
+
+				remaining -= docsEnd;
+
+				if (remaining == 0) {
+					return;
+				}
+			}
+
+			for (int i = docs.length - remaining; i < docs.length; i++) {
+				if (standbyDocuments.size() == delta) {
+					standbyDocuments.remove(0);
+					standbyScores.remove(0);
+				}
+
+				standbyDocuments.add(docs[i]);
+				standbyScores.add(hits.score(i));
+			}
+		}
+
+		protected void updateDocuments(
+			int accumulatedCount, int start, int end) {
+
+			if ((start < accumulatedCount) || standbyDocuments.isEmpty()) {
+				return;
+			}
+
+			documents.addAll(0, standbyDocuments);
+			scores.addAll(0, standbyScores);
+
+			int delta = end - start;
+			int docsStart = start - accumulatedCount;
+
+			int docsEnd = docsStart + delta;
+
+			int[] startAndEnd = SearchPaginationUtil.calculateStartAndEnd(
+				docsStart, docsEnd, documents.size());
+
+			docsStart = startAndEnd[0];
+
+			docsEnd = startAndEnd[1];
+
+			for (int i = 0; i < documents.size(); i++) {
+				if ((i < docsStart) || (i >= docsEnd)) {
+					documents.remove(i);
+					scores.remove(i);
+				}
+			}
+		}
+
+		protected void updateHits(Hits hits, int size, long startTime) {
+			hits.setDocs(documents.toArray(new Document[documents.size()]));
+			hits.setScores(ArrayUtil.toFloatArray(scores));
+			hits.setLength(size);
+			hits.setSearchTime(
+				(float)(System.currentTimeMillis() - startTime) / Time.SECOND);
+		}
+
+		protected List<Document> documents = new ArrayList<>();
+		protected List<Float> scores = new ArrayList<>();
+		protected List<Document> standbyDocuments = new ArrayList<>();
+		protected List<Float> standbyScores = new ArrayList<>();
+
+		private double _getAmplificationFactor(
+			double totalViewable, double total) {
+
+			if (totalViewable == 0) {
+				return _indexPermissionFilterSearchAmplificationFactor;
+			}
+
+			return Math.min(
+				1.0 / (totalViewable / total),
+				_indexPermissionFilterSearchAmplificationFactor);
+		}
+
+	}
 
 }