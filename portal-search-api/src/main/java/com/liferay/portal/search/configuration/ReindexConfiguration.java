brianchandotcom/liferay-portal

LPS-73059 portal-search-api: ReindexConfiguration customizes bulk sizes per model
https://issues.liferay.com/browse/LPS-73059

Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/ReindexConfiguration.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Bryan Engler
+ */
+@ExtendedObjectClassDefinition(category = "search")
+@Meta.OCD(
+	id = "com.liferay.portal.search.configuration.ReindexConfiguration",
+	localization = "content/Language", name = "reindex-configuration-name"
+)
+@ProviderType
+public interface ReindexConfiguration {
+
+	@Meta.AD(
+		deflt = "com.liferay.journal.model.JournalArticle=10000",
+		description = "indexing-batch-sizes-help",
+		name = "indexing-batch-sizes", required = false
+	)
+	public String[] indexingBatchSizes();
+
+}

vikkisy/com-liferay-portal-search

portal-search-api/src/main/java/com/liferay/portal/search/configuration/ReindexConfiguration.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Bryan Engler
+ */
+@ExtendedObjectClassDefinition(category = "foundation")
+@Meta.OCD(
+	id = "com.liferay.portal.search.configuration.ReindexConfiguration",
+	localization = "content/Language", name = "reindex-configuration-name"
+)
+@ProviderType
+public interface ReindexConfiguration {
+
+	@Meta.AD(
+		deflt = "com.liferay.journal.model.JournalArticle=10000",
+		description = "indexing-batch-sizes-help",
+		name = "indexing-batch-sizes", required = false
+	)
+	public String[] indexingBatchSizes();
+
+}